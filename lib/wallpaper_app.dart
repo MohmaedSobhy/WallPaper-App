984cae3269b26e6e7e63e8ad62b4457a32e2aa3f
Mohamed sobhy <mohamedyamany@055@gmail.com>
2023-12-11 22:30:50 +0200

    add theme to material app and routes

--- lib/wallpaper_app.dart
+++ lib/wallpaper_app.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:wall_paper/core/routes/app_routes.dart';
+import 'package:wall_paper/core/theme/app_theme.dart';
 
 class WallPaperApp extends StatelessWidget {
   const WallPaperApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.light,
+      themeMode: ThemeMode.light,
+      darkTheme: AppTheme.dark,
+      initialRoute: AppRoutes.getStarted,
+      onGenerateRoute: AppRoutes.onGenerateRoute,
+    );
   }
 }
