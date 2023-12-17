f7b521e992b3a3f3307e57f2fec50795a7326e74
Mohamed sobhy <mohamedyamany@055@gmail.com>
2023-12-17 20:43:34 +0200

    get started message

--- lib/Feature/get_started/presentation/views/get_started_message.dart
+++ lib/Feature/get_started/presentation/views/get_started_message.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import '../../../../core/localization/app_string.dart';
 
 class GetStartedMessageView extends StatelessWidget {
   const GetStartedMessageView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: MediaQuery.sizeOf(context).width * 0.05,
+      ),
+      child: Column(
+        children: [
+          Text(
+            AppString.welcomeMessage,
+            style: Theme.of(context).textTheme.headlineSmall,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            width: double.infinity,
+            height: 45,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).primaryColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              child: Text(
+                AppString.exploreMore,
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                      color: Colors.white,
+                    ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 25,
+          )
+        ],
+      ),
+    );
   }
 }
