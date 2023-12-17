ca0d04f32d7d6775cdef48ae778ffbd97ac0ca83
Mohamed sobhy <mohamedyamany@055@gmail.com>
2023-12-17 20:42:38 +0200

    refactor get started screen

--- lib/Feature/get_started/presentation/views/get_started_body_view.dart
+++ lib/Feature/get_started/presentation/views/get_started_body_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:wall_paper/core/localization/app_string.dart';
+import 'package:wall_paper/Feature/get_started/presentation/views/get_started_message.dart';
 
 import 'get_started_image.dart';
 
@@ -8,46 +8,12 @@ class GetStartedBodyView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return const Column(
       children: [
-        const Expanded(
+        Expanded(
           child: GetStartedCoverView(),
         ),
-        Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: MediaQuery.sizeOf(context).width * 0.05,
-          ),
-          child: Column(
-            children: [
-              Text(
-                AppString.welcomeMessage,
-                style: Theme.of(context).textTheme.headlineSmall,
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Theme.of(context).primaryColor,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  child: Text(
-                    AppString.exploreMore,
-                    style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                          color: Colors.white,
-                        ),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 25,
-              )
-            ],
-          ),
-        )
+        GetStartedMessageView(),
       ],
     );
   }
