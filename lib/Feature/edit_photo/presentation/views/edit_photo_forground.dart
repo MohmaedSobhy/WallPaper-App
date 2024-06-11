6b2126cb7dacaa229916cec8716c7751a025afb1
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-06-11 11:06:21 +0300

    add text widget in edit photo forground

--- lib/Feature/edit_photo/presentation/views/edit_photo_forground.dart
+++ lib/Feature/edit_photo/presentation/views/edit_photo_forground.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:wall_papper/Feature/edit_photo/presentation/controller/edit_photo_cubit.dart';
+import 'package:wall_papper/Feature/edit_photo/presentation/widgets/show_message_dialog_delete_text.dart';
 import 'package:wall_papper/core/widget/cached_network_image_widget.dart';
+
+import 'text_add_to_screen_view.dart';
 
 class EditPhotoForground extends StatelessWidget {
   final String photoUrl;
@@ -24,6 +27,18 @@ class EditPhotoForground extends StatelessWidget {
             color: Colors.black.withOpacity(
               EditPhotoCubit.getInstanse().imageOpacity,
             ),
+          ),
+        ),
+        Visibility(
+          visible: EditPhotoCubit.getInstanse().textWidgetIsVisiable,
+          child: TextAddToScreenView(
+            onTap: () {
+              showMessageSureToDelteTextDialoge(context).then((value) {
+                if (value) {
+                  EditPhotoCubit.getInstanse().textWidgetVisiable(false);
+                }
+              });
+            },
           ),
         ),
       ],
