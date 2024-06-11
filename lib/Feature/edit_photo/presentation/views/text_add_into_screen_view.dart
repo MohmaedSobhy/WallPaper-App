393c9859bcf2be6a24e1790741ec1835905451fc
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-06-11 11:28:36 +0300

    refactor text to text field

--- lib/Feature/edit_photo/presentation/views/text_add_into_screen_view.dart
+++ lib/Feature/edit_photo/presentation/views/text_add_into_screen_view.dart
@@ -3,27 +3,31 @@ import 'package:wall_papper/Feature/add_text_layout/controller/add_text_layout_c
 
 class TextAddToScreenView extends StatelessWidget {
   final VoidCallback onTap;
+
   const TextAddToScreenView({super.key, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
+    return Positioned.fill(
       top: AddTextLayoutCubit.instanse.top,
       left: AddTextLayoutCubit.instanse.left,
       child: InkWell(
         onDoubleTap: onTap,
         child: SizedBox(
-          child: Text(
-            AddTextLayoutCubit.instanse.textEditingController.text.toString(),
-            maxLines: 5,
-            style: AddTextLayoutCubit.instanse.textStyle.copyWith(
-              color: AddTextLayoutCubit.instanse.texColor,
-              fontSize: AddTextLayoutCubit.instanse.textFontSize,
-              fontStyle: AddTextLayoutCubit.instanse.textFontStyle,
-              fontWeight: AddTextLayoutCubit.instanse.textFontWeight,
-            ),
+            child: TextField(
+          enabled: false,
+          maxLines: 10,
+          controller: AddTextLayoutCubit.instanse.textEditingController,
+          style: AddTextLayoutCubit.instanse.textStyle.copyWith(
+            color: AddTextLayoutCubit.instanse.texColor,
+            fontSize: AddTextLayoutCubit.instanse.textFontSize,
+            fontStyle: AddTextLayoutCubit.instanse.textFontStyle,
+            fontWeight: AddTextLayoutCubit.instanse.textFontWeight,
           ),
-        ),
+          decoration: const InputDecoration(
+            border: InputBorder.none,
+          ),
+        )),
       ),
     );
   }
